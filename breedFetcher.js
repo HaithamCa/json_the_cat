619f1fee30330f8909f5300275f84702a7449b52
hytham defrawy <hythamdefrawy@gmail.com>
2021-11-04 21:10:32 -0400

    Made a breedfetcher test and some changes into breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,28 @@
-const request = require('request')
-const catName = process.argv[2]
+const request = require('request');
 
- const domain = "https://api.thecatapi.com/v1/breeds/search?q=" + catName;
- request(domain, (error, response, body) => {
-   const data = JSON.parse(body);
-   console.log(data)
-   if (error) {
-     console.log('error:', error);
-   } 
-    if (data.length === 0) {
-     console.log(`There is no ${catName} `);
-   } else {
-     console.log(data[0].description);
-   }
- }); 
+const fetchBreedDescription = function(breedName, callback) {
+  const domain = "https://api.thecatapi.com/v1/breeds/search?q=" + breedName;
+  //   console.log(domain)
+  request(domain, (error, response, body) => {
+      
+    if (error) {
+      return callback(error, null);
+    }
+    const data = JSON.parse(body);
+    // console.log(data[0])
+    if (data[0]) {
+      //   console.log(data[0])
+      // error = `There is no ${breedName}`;
+    //    callback(null , "There is no breed exist");
+    //   console.log(`There is no ${breedName}`);
+      console.log(data[0].description);
+      callback(null, data[0].description);
+    } else  {
+      callback('This breed is not found');
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
+
+ 
